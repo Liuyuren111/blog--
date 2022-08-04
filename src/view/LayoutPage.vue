5db128667eb8d9679d75ccef873ca5370d9062ed
liuyuren <1540979440@qq.com>
2022-08-04 18:35:50 +0800

    fix: 项目初始化

--- src/view/LayoutPage.vue
+++ src/view/LayoutPage.vue
@@ -2,14 +2,22 @@
     <div>首页</div>
     <el-button>Default</el-button>
     <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
+    <el-button type="success" @click="handleSuccess">Success</el-button>
     <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
+    <el-button type="warning" @click="handleWarning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
     <el-button>中文</el-button>
 </template>
 
 <script setup>
+import { message } from '@/utils/message';
+
+const handleSuccess = () =>{
+    message('成功', 'success')
+}
+const handleWarning = () =>{
+    message('警告', 'warning')
+}
 </script>
 
 <style lang='scss' scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import legacyPlugin from '@vitejs/plugin-legacy' // IE11和旧版chrome兼容
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,26 +14,35 @@ export default defineConfig({
     vue(),
     vueJsx(),
     eslintPlugin({
-      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"],
+    }),
+    legacyPlugin({
+      targets: ['chrome 52'], // 需要兼容的目标列表，可以设置多个
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'] // 面向IE11时需要此插件
     }),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    })
+    }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
     strictPort: true, // 设为true时若端口已被占用则会直接退出，而不是尝试下一个可用端口
-    https: '',
+    https: "",
     open: false,
     proxy: {
-      '/api': {
-        target: 'http://jsonplaceholder.typicode.com',
+      "/api": {
+        target: "http://jsonplaceholder.typicode.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
